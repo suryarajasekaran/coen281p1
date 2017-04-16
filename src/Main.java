035c47334affbc0ef1a920c1f4334af327aa32ab
skasabar <skasabar@cisco.com>
2017-04-16 00:33:31 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ public class Main {
 
     public static void main(String[] args){
 
-        DocumentsProcessor documentsProcessor = new DocumentsProcessor("/Users/SuryaRajasekaran/scratch/coen281p1/input/f1Surya.txt",
+
+
+        for (String s: args) {
+            System.out.println(s);
+        }
+
+        /*DocumentsProcessor documentsProcessor = new DocumentsProcessor("/Users/SuryaRajasekaran/scratch/coen281p1/input/f1Surya.txt",
                                                                     "/Users/SuryaRajasekaran/scratch/coen281p1/input/f1.txt",
                                                                     "/Users/SuryaRajasekaran/scratch/coen281p1/input/f2.txt",
                                                                     "/Users/SuryaRajasekaran/scratch/coen281p1/input/f3.txt",
@@ -16,7 +22,7 @@ public class Main {
 
 
 
-        documentsProcessor.processDocuments();
+        documentsProcessor.processDocuments();*/
 
 
     }
