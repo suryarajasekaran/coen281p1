16e95a6880ccabd06acef51953a9e38709d7ff16
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2017-04-16 14:21:38 -0700

    updated main

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,27 @@ public class Main {
     public static void main(String[] args){
 
         for (String detailFilePath : args) {
+            System.out.println("======================================================================");
+            System.out.format("Processing Input File : %s", detailFilePath);
+            System.out.println();
             FileCheck fileCheck = new FileCheck(detailFilePath);
             if (fileCheck.isFile(detailFilePath) == true) {
-                if (fileCheck.checkInnerFiles() == true) {
-                    if (fileCheck.getInnerFiles() != null) {
-                        DocumentsProcessor documentsProcessor = new DocumentsProcessor(fileCheck.getInnerFiles());
-                        documentsProcessor.processDocuments();
-                    } else {
-                        System.out.format("input file %s data inner files are invalid", detailFilePath);
+                if ((fileCheck.checkInnerFiles() == true) && (fileCheck.getInnerFiles() != null)) {
+                    System.out.println("-----------------------------------");
+                    System.out.println("Input File Mapping");
+                    for (int i=0; i<fileCheck.getInnerFiles().length; i++){
+                         System.out.println("D" + i + " : " + fileCheck.getInnerFiles()[i]);
                     }
+                    DocumentsProcessor documentsProcessor = new DocumentsProcessor(fileCheck.getInnerFiles());
+                    documentsProcessor.processDocuments();
                 }
                 else {
-                    System.out.format("input file %s data is invalid", detailFilePath);
+                    System.out.println("-----------------------------------");
+                    System.out.format("ERROR : input file %s inner data files are invalid", detailFilePath);
                 }
             } else {
-                System.out.format("input file %s is invalid", detailFilePath);
+                System.out.println("-----------------------------------");
+                System.out.format("ERROR : input file %s is invalid OR does not exist OR is not readable", detailFilePath);
             }
         }
     }
