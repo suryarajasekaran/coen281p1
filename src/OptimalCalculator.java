4e56e6d365636b91e8f1be97256bf1a5e43638f1
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2017-04-16 21:19:16 -0700

    update

---
+++ src/OptimalCalculator.java
@@ -0,0 +1,26 @@
+/**
+ * Created by SuryaRajasekaran on 4/16/17.
+ */
+public class OptimalBandRowCalculator {
+
+    float d1;
+    float d2;
+    float p1;
+    float p2;
+
+    public OptimalBandRowCalculator(float d1, float d2, float p1, float p2) {
+        this.d1 = d1;
+        this.d2 = d2;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    /*
+    public getBands() {
+
+    }
+
+    public getRows() {
+
+    }*/
+}
